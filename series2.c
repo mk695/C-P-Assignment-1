b5cb886a73347c4b4920a2a650e6babee4b3f46d
mk695 <62099445+mk695@users.noreply.github.com>
2020-03-13 12:44:03 +0530

    series2.c

--- series2.c
+++ series2.c
@@ -2,11 +2,9 @@
  * C Program to find the Sum of Series 1 + 1/2 + 1/3 + 1/4 + ... + 1/N
  */
 #include <stdio.h>
- 
 void main()
 {
     double number, sum = 0, i;
- 
     printf("\n enter the number ");
     scanf("%lf", &number);
     for (i = 1; i <= number; i++)
