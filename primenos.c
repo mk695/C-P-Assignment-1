1018dcb87d26c456069623fe226d26ba8dc3deb0
mk695 <62099445+mk695@users.noreply.github.com>
2020-03-13 12:21:25 +0530

    prime.c

--- primenos.c
+++ primenos.c
@@ -1,11 +1,8 @@
 /* C Program to Print Prime Numbers between 1 to 100 using For Loop */
- 
 #include <stdio.h>
- 
 int main()
 {
   int i, Number, c; 
-  
   printf(" Prime Number from 1 to 100 are: \n"); 
   for(Number = 1; Number <= 100; Number++)
   {
